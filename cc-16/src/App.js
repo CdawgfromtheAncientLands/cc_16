d2e0574c7e1f2b706f0e8fe6162dc54c25b682e0
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-07-09 22:07:26 -0400

    Adjust App.js and add simple CSS file

--- cc-16/src/App.js
+++ cc-16/src/App.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import Gallery from './Gallery';
+import './App.css';
 
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+      </header>
+      <Gallery />
+    </div>
+  );
+};
 
 export default App;
