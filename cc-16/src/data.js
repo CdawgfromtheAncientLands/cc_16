477bbc32a0fe3719333113c25ef4fb28277cfc7a
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-07-09 22:12:21 -0400

    Added pictures to data and adjusted information

--- cc-16/src/data.js
+++ cc-16/src/data.js
@@ -1,6 +1,6 @@
-const memberID1 = ["Alice", "Developer", "/path/to/alice.jpg"];
-const memberID2 = ["Bob", "Designer", "/path/to/bob.jpg"];
-const memberID3 = ["Charlie", "Manager", "/path/to/charlie.jpg"];
+const memberID1 = ["Alice 0", "Super Duper Developer", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fphotos%2Fprofessional-woman.html&psig=AOvVaw3IerN53tt8ixAFGzo-TlBK&ust=1720663679656000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKiqiO2xm4cDFQAAAAAdAAAAABAEhttps://st5.depositphotos.com/1049680/66659/i/450/depositphotos_666597232-stock-photo-young-hispanic-woman-wearing-business.jpg"];
+const memberID2 = ["Alice 1", "Designer of Evil Tricks", "https://st5.depositphotos.com/1049680/66659/i/450/depositphotos_666597232-stock-photo-young-hispanic-woman-wearing-business.jpg"];
+const memberID3 = ["Alicia", "Cruel Torturer", "https://st5.depositphotos.com/1049680/66659/i/450/depositphotos_666597232-stock-photo-young-hispanic-woman-wearing-business.jpg"];
 
 const masterArray = [memberID1, memberID2, memberID3];
 
