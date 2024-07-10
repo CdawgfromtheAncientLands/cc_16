503c574d4fdec8dbc7e64b18d4be8cd1b5303042
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-07-09 22:00:06 -0400

    Added profile component

---
+++ cc-16/src/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Profile.css'; //file currently nonexistent
+
+const Profile = ({ name, role, picture }) => {
+  return (
+    <div className="profile">
+      <img src={picture} alt={name} className="profile-picture" />
+      <div className="profile-name">{name}</div>
+      <div className="profile-role">{role}</div>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+};
+
+export default Profile;
