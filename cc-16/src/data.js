55dfa2ff9dedf2de1b60313db61c1cca920a2d63
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-07-09 21:58:23 -0400

    Added file with member information & placeholder information

---
+++ cc-16/src/data.js
@@ -0,0 +1,7 @@
+const memberID1 = ["Alice", "Developer", "/path/to/alice.jpg"];
+const memberID2 = ["Bob", "Designer", "/path/to/bob.jpg"];
+const memberID3 = ["Charlie", "Manager", "/path/to/charlie.jpg"];
+
+const masterArray = [memberID1, memberID2, memberID3];
+
+export { masterArray };
